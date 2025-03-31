5b25047e38d99e019857755eae3528f862fd8077
Sri Lakshmi Polavarapu <srilakshmi.polavarapu01@gmail.com>
2025-03-31 01:04:30 -0700

    added code for editing the info

--- src/main/java/full_stack/full_stack_project/controller/UserController.java
+++ src/main/java/full_stack/full_stack_project/controller/UserController.java
@@ -7,12 +7,13 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import full_stack.full_stack_project.exception.UserNotFoundException;
 import full_stack.full_stack_project.model.User;
 import full_stack.full_stack_project.repository.UserRepository;
-import full_stack.full_stack_project.exception.UserNotFoundException;
 
 
 @RestController
@@ -39,6 +40,17 @@ public class UserController {
     {
         return userRepository.findById(id)
             .orElseThrow(() -> new UserNotFoundException(id));
+    }
+
+    @PutMapping("/user/{id}")
+    User updateUser(@RequestBody User newUser, @PathVariable Long id) {
+        return userRepository.findById(id)
+                .map(user -> {
+                    user.setUsername(newUser.getUsername());
+                    user.setName(newUser.getName());
+                    user.setEmail(newUser.getEmail());
+                    return userRepository.save(user);
+                }).orElseThrow(() -> new UserNotFoundException(id));
     }
 
 }
